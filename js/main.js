d5b3f2827a7ce87bd3eae5ee0726229388d86a08
Misael <oreb.ruiz@gmail.com>
2022-02-26 10:32:55 -0500

    second commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+var list =document.querySelectorAll(".navigation li");
+
+function activeLink(){
+	list.forEach((item)=>item.classList.remove('hovered'));
+	this.classList.add('hovered');
+
+}
+
+list.forEach((item)=>item.addEventListener('mouseover',activeLink))
+
